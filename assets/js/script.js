973cd08f432e24bb8ffc797b77805bb9759c85e6
Herdyanto26 <bherdyanto26@gmail.com>
2023-03-01 07:40:25 +0700

    few fix and upgrade
    beberapa fix dan tambahan pada navigasi bar.
    
    nav icon added!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-$('nav li').hover(
-    $( document ).ready(function() {
-            $('ul', this).stop().slideDown(200);
-          },
-          function() {
-            $('ul', this).stop().slideUp(200);
-          }
-        ));
+
+
+
