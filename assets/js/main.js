d6180125574503a46111153187b42f173ab01911
Herdyanto26 <bherdyanto26@gmail.com>
2023-03-01 23:40:32 +0700

    feature update
    back to top button added
    jquerry show/hide button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+
+
+/* tidak berfungsi, di kerjakan nanti
 function openTab(evt, projName) {
 
     var i, isiTab, tablink;
@@ -19,4 +22,53 @@ function openTab(evt, projName) {
     document.getElementById("active").click();
   } 
 
+   */
 
+  const navbar = document.querySelector('.navbar');
+
+      window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+          navbar.classList.add('navbar-scrolled', 'dropdown-menu-scrolled', 'konten-drop-transform');
+        } else {
+          navbar.classList.remove('navbar-scrolled', 'dropdown-menu-scrolled', 'konten-drop-transform');
+        }
+      });
+
+  $(document).ready(function() { $('.post-card').hover(
+      function() {
+        $(this).addClass('hovered');
+      },
+      function() {
+        $(this).removeClass('hovered');
+      }
+    );
+  });
+
+
+  $(document).ready(function(){
+    $(".tmbl1").click(function(){
+      $(".youtube").hide();
+    });
+    $(".tmbl2").click(function(){
+      $(".youtube").show();
+    });
+  });
+    
+
+  let mybutton = document.getElementById("goUp");
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+/* untuk spesifik browser **/
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+} 
+
+  
