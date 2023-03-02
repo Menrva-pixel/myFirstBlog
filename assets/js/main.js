0edc625ad046fc8380cdc576baad5b173c4df413
Herdyanto26 <bherdyanto26@gmail.com>
2023-03-02 20:43:52 +0700

    Major update
    many fixes and feature update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,4 @@
-/* tidak berfungsi, di kerjakan nanti
-function openTab(evt, projName) {
-
-    var i, isiTab, tablink;
-  
-
-    isiTab = document.getElementsByClassName("isiTab");
-    for (i = 0; i < isiTab.length; i++) {
-      isiTab[i].style.display = "none";
-    }
-  
-    tablink = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablink.length; i++) {
-      tablink[i].className = tablink[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(projName).style.display = "block";
-    evt.currentTarget.className += " active";
-
-    document.getElementById("active").click();
-  } 
-
-   */
+/*======(c)copyright Barkah Herdyanto Sejati============*/
 /* untuk transform style semua element yang ada di navbar */
 const navbar = document.querySelector(".navbar");
 
